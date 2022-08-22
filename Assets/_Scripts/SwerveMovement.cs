44fe1a658ec4dea9bb34a2fb8aee7a140d9bd3f2
suedas <55928205+suedas@users.noreply.github.com>
2022-08-22 13:35:15 +0300

    platform

--- Assets/_Scripts/SwerveMovement.cs
+++ Assets/_Scripts/SwerveMovement.cs
@@ -73,9 +73,11 @@ public class SwerveMovement : MonoBehaviour
 
             var x = transform.position.x + swerve;
             if (x < maxHorizontalDistance && x > -maxHorizontalDistance)
+                transform.Rotate(90, 0, 0);
                 if (checkDistanceChange)
                 {
                     if (Mathf.Abs(x - lastPositonChange) < maxHorizontalChange) transform.Translate(swerve, 0, 0);
+            
                 }
                 else
                     transform.Translate(swerve, 0, 0);
